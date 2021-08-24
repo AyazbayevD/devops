5633d14f2429a47e44e76809f3e325d1e09bebf0
Данат Аязбаев <danat.ayazbayev@gmail.com>
2021-08-24 20:36:14 +0300

    moscow time app

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    tz_moscow = pytz.timezone('Europe/Moscow')
+    datetime_moscow = datetime.now(tz_moscow)
+    moscow_time = datetime_moscow.strftime("%H:%M:%S")
+    return f"<p>CURRENT MOSCOW TIME: {moscow_time}<p>"
